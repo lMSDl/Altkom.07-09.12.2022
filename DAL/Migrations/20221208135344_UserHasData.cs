790323a455bbb7d8064d750ed2afddaa9b3daf07
Paweł Łęczycki <pawel@msd.info.pl>
2022-12-08 14:53:57 +0100

    Seed

---
+++ DAL/Migrations/20221208135344_UserHasData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class UserHasData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Username", "Password", "UserType" },
+                values: new object[] { "SuperAdmin", "bmltZEE=", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Username", "Password", "UserType" },
+                values: new object[] { "User", "cmVzVQ==", "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Username",
+                keyValue: "SuperAdmin");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Username",
+                keyValue: "User");
+        }
+    }
+}
